294b6045017dc528759e4db54249bdc0f81770a5
abhay_pandit <abhaypanditzx@gmail.com>
2025-02-22 12:23:49 +0530

    Updated frontend files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+
+import react, { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([])
+  const sendMessage = async () => {
+    if (!message.trim()) return;
+    const newChat = [...chat, { sender: "you", text: message }];
+    setChat(newChat);
+    try {
+      const response = await axios.post("http://localhost:5000/chat", { message })
+      setChat([...newChat, { sender: "bot", text: response.data.reply }]);
+      setMessage("")
+    } catch (err) {
+      console.error("error:", err)
+      setChat([...newChat, { sender: "bot", text: "error: unable to get response" }])
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className="app">
+     <div className="chat-container">
+       < div className="nav">
+           <div className="logo">BOT</div>
+       </div>
+      <div className="chat-box">
+        {chat.map((c,index)=>(
+          <p key={index} className={c.sender === "you" ? "user-message" : "bot-message"}>
+            <strong>{c.sender}:</strong>{c.text}
+
+          </p>
+        ))}
+      </div>
+      <div className="send-container">
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message} />
+        <button onClick={sendMessage}>send</button>
+      </div>
     </div>
+ </div>
   );
 }
 
